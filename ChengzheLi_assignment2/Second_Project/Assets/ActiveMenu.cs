186541e51ff756eda1061aa1c986ebea7999e68c
Chengzhe Li <smalllicheng@gmail.com>
2020-02-19 15:22:58 -0500

    Upload Project 1 and 2

---
+++ ChengzheLi_assignment2/Second_Project/Assets/ActiveMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveMenu : MonoBehaviour {
+
+
+    public GameObject canavas;
+    public Camera cam1, cam2,currCam;
+    public GameObject ball1, ball2;
+    public Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+        currCam = Camera.main;
+        cam2.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (!canavas.activeSelf)
+            {
+                canavas.SetActive(true);
+                ball1.SetActive(false);
+                ball2.SetActive(false);
+            }
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            if (canavas.activeSelf == false)
+            {
+                if (cam1.enabled == true)
+                {
+                    cam1.enabled = false;
+                    cam2.enabled = true;
+                    currCam = cam2;
+                }
+                else
+                {
+                    cam2.enabled = false;
+                    cam1.enabled = true;
+                    currCam = cam1;
+                }
+            }
+            
+        }
+
+
+        if (ball2.activeSelf)
+        {
+            position = currCam.ScreenToWorldPoint(Input.mousePosition + new Vector3(0,0,452));
+            if(position.y <= 75)
+            {
+                ball2.transform.localScale = new Vector3(100,position.y+25,100);
+            }
+
+            ball2.transform.position = position;
+            //Debug.Log(position);
+        }
+	}
+}
---
+++ ChengzheLi_assignment2/Second_Project/Assets/Scenes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Scenes : MonoBehaviour {
+    public Button button_1;
+    public Button button_2;
+    public GameObject canavas;
+    public GameObject ball1;
+    public GameObject ball2;
+
+    public void ss1()
+    {
+        ball1.SetActive(true);
+        canavas.SetActive(false);
+    }
+
+    public void ss2()
+    {
+        ball2.SetActive(true);
+        canavas.SetActive(false);
+    }
+
+    // Use this for initialization
+    void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        button_1.onClick.AddListener(ss1);
+        button_2.onClick.AddListener(ss2);
+    }
+}
